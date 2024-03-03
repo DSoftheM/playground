1dd1fb1730a98340489c57d3ed10db1012839296
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-03-03 15:23:48 +0500

    f

--- client/src/auth/login-form.tsx
+++ client/src/auth/login-form.tsx
@@ -1,10 +1,9 @@
-import { Button, Form, Input, Typography } from "antd";
+import { Button, Flex, Form, Input, Typography } from "antd";
 import { useState } from "react";
 import { useLoginMutation } from "./use-login-mutation";
 import { useProfileQuery } from "./use-profile-query";
 import { Navigate } from "react-router-dom";
 import { nav } from "../navigation/nav";
-import { motion } from "framer-motion";
 
 export function LoginForm() {
     const [login, setLogin] = useState("1");
@@ -17,25 +16,27 @@ export function LoginForm() {
     }
 
     return (
-        <Form layout="vertical" onValuesChange={console.log}>
-            <Typography.Title>Вход в систему</Typography.Title>
-            <Form.Item label="Логин">
-                <Input name="login" value={login} onChange={(e) => setLogin(e.target.value)} />
-            </Form.Item>
-            <Form.Item label="Пароль">
-                <Input name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </Form.Item>
-            <Form.Item>
-                <Button
-                    type="primary"
-                    htmlType="submit"
-                    disabled={loginMutation.isLoading || profileQuery.isRefetching}
-                    onClick={() => loginMutation.mutate({ login, password })}
-                >
-                    Войти
-                </Button>
-            </Form.Item>
-            {loginMutation.isError && <Typography.Text type="danger">{loginMutation.error.response?.data}</Typography.Text>}
-        </Form>
+        <Flex align="center" justify="center" style={{ height: "100vh" }}>
+            <Form layout="vertical" style={{ flexBasis: "400px", margin: "0 10px" }}>
+                <Typography.Title>Вход в систему</Typography.Title>
+                <Form.Item label="Логин">
+                    <Input name="login" value={login} onChange={(e) => setLogin(e.target.value)} />
+                </Form.Item>
+                <Form.Item label="Пароль">
+                    <Input name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        disabled={loginMutation.isLoading || profileQuery.isRefetching}
+                        onClick={() => loginMutation.mutate({ login, password })}
+                    >
+                        Войти
+                    </Button>
+                </Form.Item>
+                {loginMutation.isError && <Typography.Text type="danger">{loginMutation.error.response?.data}</Typography.Text>}
+            </Form>
+        </Flex>
     );
 }
